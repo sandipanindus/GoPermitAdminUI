31f93df53fff3cb427be8f3a47bf031d9e1eb815
s-md-ashfan <shaik.ashfan@fadelsoft.co.in>
2025-02-20 14:58:08 +0530

    commit
    change password api integration

--- Vien-Angular/src/app/views/app/app.module.ts
+++ Vien-Angular/src/app/views/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutContainersModule } from 'src/app/containers/layout/layout.containers.module';
 import { PagePasswordComponent } from './page-password/page-password.component';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule
+import { SimpleNotificationsModule } from 'angular2-notifications'; // Import this
+
+
 
 @NgModule({
   declarations: [BlankPageComponent, AppComponent, PagePasswordComponent],
@@ -13,7 +17,10 @@ import { PagePasswordComponent } from './page-password/page-password.component';
     CommonModule,
     AppRoutingModule,
     SharedModule,
-    LayoutContainersModule
+    LayoutContainersModule,
+    FormsModule,
+    SimpleNotificationsModule.forRoot(), // Add this line
+
   ]
 })
 export class AppModule { }
